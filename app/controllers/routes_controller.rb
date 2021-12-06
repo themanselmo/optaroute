f116749b0b9ce8559d77193a038b32eb8e7ab106
max anselmo <itmanselmo@gmail.com>
2021-12-06 16:58:23 -0500

    completed controller methods for the routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -5,4 +5,49 @@ class RoutesController < ApplicationController
 
         render json: routes
     end 
+
+    def create 
+        route = Route.create(route_params)
+
+        if route.valid? 
+            render json: route, status: :created
+        else
+            render json: {error: route.errors.full_messages}, status: :unprocessable_entity
+        end
+
+    end
+
+    def update
+        route = Route.find_by(id: params[:id])
+        if route
+            route.update(route_params)
+        if route.valid?
+            render json: route,status: :ok
+        else
+            render json:{ error: route.errors.full_messages}, status: :unprocessable_entity
+        end
+        else
+            render json: {error: "Not found"}, status: :not_found
+        end
+    end
+
+    def destroy
+        route = Route.find_by(id: params[:id])
+        if route 
+            route.destroy 
+            head :no_content
+        else
+            render json: {error: "Not found"}, status: :not_found
+        end
+    end
+
+    def route_params
+        params.permit(
+            :starting_point,
+            :destination,
+            :distance,
+            :date,
+            :user_id
+        )
+    end
 end
