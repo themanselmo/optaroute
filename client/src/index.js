a9ce1ccb2931dde2b1190538a7fefbcfffcd2f8d
max anselmo <itmanselmo@gmail.com>
2021-12-08 09:16:53 -0500

    small changes

--- client/src/index.js
+++ client/src/index.js
@@ -8,12 +8,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    {/* <Routes>
-      try moving routes to app and use the graphspage rather than having them in app
-      <Route path="/Routes" element={<RoutesPage />}/>
-      <Route path="/Home" element={<App />} />
-      <Route path="/" element={<App />} />
-    </Routes> */}
     <App />
     </BrowserRouter>,
   document.getElementById('root')
